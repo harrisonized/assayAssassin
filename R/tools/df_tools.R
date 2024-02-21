a3baace219eef1e95eba1a696caf7a9a3d9d0eab
Harrison Wang <harrisonized@gmail.com>
2024-02-21 01:18:01 -0500

    add 20240220-plot_qpcr.R, which features dct of dnase1l1 vs actin

--- R/tools/df_tools.R
+++ R/tools/df_tools.R
@@ -1,8 +1,29 @@
 ## Functions
+## set_index
 ## reset_index
+## rev_df
+## smelt
+## df_to_plate
 
 
-#' Reset index
+#' Set Index
+#' 
+#' @description
+#' Uses the values in a column to set an index. Needs to be unique.
+#' Mirrors Pandas' \href{https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.set_index.html}{set_index}.
+#' 
+#' @export
+set_index <- function(df, colname, drop=TRUE) {
+
+    rownames(df) <- df[[colname]]
+    if (drop) {
+        df <- df[, !names(df)==colname]
+    }
+    return(df)
+}
+
+
+#' Reset Index
 #' 
 #' @description
 #' Moves the values in the index to a column. Resets the index to the default integer index.
@@ -27,4 +48,73 @@ reset_index <- function(df, index_name='index', drop=FALSE) {
     }
     rownames(df) <- 1:nrow(df)
     return (df)
+}
+
+
+#' Reverse the order of a dataframe
+#' 
+#' @param df a dataframe
+#' @param how 'row' or 'col'
+#' @return Returns the reversed dataframe.
+#' 
+#' @examples
+#' rev_df(mtcars, how='row')
+#' rev_df(mtcars, how='col')
+#' 
+#' @export
+rev_df <- function(df, how='row') {
+    if (how == 'row') {
+        return(df[dim(df)[1]:1,])
+    } else if (how == 'col') {
+        return(rev(df))
+    } else {
+        stop("Choose how='row' or how='col'")
+    }
+}
+
+
+#' Special Melt
+#' 
+#' @description
+#' Convert a dataframe from a table to long format
+#' This is an alternative to melt that doesn't throw errors
+#' 
+#' @examples
+#' smelt(mtcars[, c('cyl', 'mpg')])
+#' 
+#' @references
+#' See: \href{https://stackoverflow.com/questions/28355377/how-to-add-index-of-a-list-item-after-melt-in-r}{Stack Overflow link}
+#' 
+#' @export
+smelt <- function(
+   df,
+   rowname='row',
+   colname='col',
+   valname='val'
+) {
+   melted <- transform(stack(setNames(df, colnames(df))), id=rownames(df))
+   colnames(melted) <- c(valname, colname, rowname)
+   return(rev(melted))
+}
+
+
+#' Dataframe to Plate
+#' 
+#' @description
+#' Reshape a column dataframe to a plate format
+#' 
+#' @export
+df_to_plate <- function(df, well_id='well_position', value='ct') {
+
+    tmp <- df[, c(well_id, value)] %>%
+        group_by(!!sym(well_id)) %>%
+        summarize(!!(paste0('mean_', value)) := mean(!!sym(value), na.rm=TRUE))
+    tmp[['row']] = lapply(tmp[well_id], function(x) gsub("[^a-zA-Z]", "", x))[[1]]
+    tmp[['col']] = as.numeric(lapply(tmp[well_id], function(x) gsub("[^0-9]", "", x))[[1]])
+    tmp <- tmp[order(tmp$row, tmp$col),]
+
+    plate <- as.data.frame(pivot_wider(tmp, id_cols='row', names_from='col', values_from=paste0('mean_', value)))
+    plate <- set_index(plate, colname='row')
+
+    return(plate)
 }
--- R/tools/plotting.R
+++ R/tools/plotting.R
@@ -1,7 +1,12 @@
 import::here(rlang, 'sym')
 import::here(ggplot2,
-    'ggplot', 'aes', 'theme', 'labs',  'geom_bar',
-    'guide_axis', 'scale_x_discrete')
+    'ggplot', 'aes', 'aes_string', 'theme', 'labs', 
+    'geom_bar', 'geom_tile', 'geom_text', 'coord_fixed',
+    'guide_axis', 'scale_x_discrete', 'scale_y_discrete', 'scale_fill_gradient',
+    'element_text', 'element_blank')
+import::here(file.path(wd, 'R', 'tools', 'df_tools.R'),
+    'smelt', .character_only=TRUE)
+
 
 ## Functions
 ## plot_bar
@@ -51,3 +56,88 @@ plot_bar <- function(
 
     return(fig)
 }
+
+
+#' Plot a heatmap
+#'
+#' @usage
+#' plot_heatmap(
+#'   df,
+#'   title="Raw Data",
+#'   show_xlabel=TRUE,
+#'   show_ylabel=TRUE,
+#'   annotations=TRUE,
+#'   scientific_notation=FALSE,
+#'   digits=0
+#' )
+#' 
+#' @section Vignette:
+#' See `vignettes/plot_heatmap.Rmd`
+#' 
+#' @export
+plot_heatmap <- function(
+   df,
+   x='col',
+   y='row',
+   fill='val',
+   title=NULL,
+   show_xlabel=TRUE,
+   show_ylabel=TRUE,
+   annotations=FALSE,
+   scientific_notation=FALSE,
+   digits=1
+) {
+    
+    tab <- smelt(rev_df(df))  # reshape
+
+    # axis labels
+    if (show_xlabel) {
+        xlabel = element_text()
+    } else {
+        xlabel = element_blank()
+    }
+    if (show_ylabel) {
+        ylabel = element_text()
+    } else {
+        ylabel = element_blank()
+    }
+
+    # annotations
+    if (annotations) {
+        label = 'label'
+        if (scientific_notation) {
+            tab['label'] = lapply(
+                tab['val'], 
+                function(x) formatC(x, format='e', digits=2)
+            )
+        } else {
+            tab['label'] = lapply(
+                tab['val'], 
+                function(x) round(x, digits)
+            )
+        }
+    } else {
+        label = NULL
+    }
+
+    # plot
+    fig <- ggplot(tab, aes_string(x=x, y=y, fill=fill)) +
+        geom_tile(color="white", lwd=0.3, linetype=1) +
+        coord_fixed(expand=TRUE) +
+        scale_y_discrete(limits=rev) +
+        labs(title = title) +
+        theme(plot.title = element_text(size = 10),
+                       axis.title.x = xlabel,
+                       axis.title.y = ylabel) +
+        scale_fill_gradient(low="#FFF8F8", high="#A50026") +
+        if (annotations) {
+            geom_text(
+                aes_string(label=label),
+                color = 'black',
+                size = 2
+            )
+        }
+
+    return (fig)
+}
+
